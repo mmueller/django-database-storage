39d913978cf24045d496fba778c5b83c0a528111
Mike Mueller <mike@subfocal.net>
2011-10-29 04:09:01 -0700

    Bump version to 0.1.1.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='django-database-storage',
-    version='0.1.0',
+    version='0.1.1',
     packages=['database_storage'],
     license='BSD',
     long_description=open('README').read(),
